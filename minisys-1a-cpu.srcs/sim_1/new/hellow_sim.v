8d1d82d3522dc2c9bce0af37b73d697a65daef3a
LostBlackCat <wxy1999814@gmail.com>
2020-10-15 12:32:05 +0800

    first commit

---
+++ minisys-1a-cpu.srcs/sim_1/new/hellow_sim.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/10/15 12:07:52
+// Design Name: 
+// Module Name: hellow_sim
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module hellow_sim( );
+    //input
+    reg [7:0] sw = 8'h00;
+    //output
+    wire [7:0] led;
+    //instantiate the Unit under test
+    Ex_1 uut(
+        .sw(sw),
+        .led(led)
+    );
+    always #10 sw = sw+1;
+endmodule
---
+++ minisys-1a-cpu.srcs/sources_1/new/hellow.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2020/10/15 12:03:54
+// Design Name: 
+// Module Name: hellow
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module hellow(
+    input[7:0] sw,
+    output[7:0] led
+    );
+    assign led = sw;
+endmodule
