b9e34f61cd0fae6dbc85b6538b27fead73037efc
z0gSh1u <zx.cs@qq.com>
2020-12-22 20:56:38 +0800

    补充数码管、计时器等模块占位

---
+++ minisys-1a-cpu.srcs/sources_1/interface/digits.v
@@ -0,0 +1,11 @@
+// digits.v
+// https://github.com/seu-cs-class2/minisys-1a-cpu
+
+`include "public.v"
+
+// 八位七段共阳极数码管
+module digits (
+  
+);
+
+endmodule
---
+++ minisys-1a-cpu.srcs/sources_1/interface/uart.v
@@ -0,0 +1,12 @@
+// uart.v
+// https://github.com/seu-cs-class2/minisys-1a-cpu
+
+`include "public.v"
+
+// UART 串口通讯
+module uart (
+
+);
+
+
+endmodule
